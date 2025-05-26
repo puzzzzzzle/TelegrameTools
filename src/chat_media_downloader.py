8ca3c99cbef6cdf4b0ec2b8ed4d6eb621f335fe8
puzzzzzzle <2359173906@qq.com>
2025-05-26 21:17:22 +0800

    update

--- src/chat_media_downloader.py
+++ src/chat_media_downloader.py
@@ -105,16 +105,15 @@ class MediaDownloadTask(object):
         self.no_data_recv_time = no_data_recv_time
         self.msg_id = msg_id
         self.chat_id = chat_id
-
-        # task_id, task_tag, start_time,last_recv_time,  recv_bytes, total_bytes
-        self.on_downloader_net_callback: Callable[[int, str, datetime.datetime, datetime.datetime, int,
-                                                   int], None] | None = None
         self.task_id = None
         self.chat_name = chat_name
         self.file_name = file_name
         self.message = message
         self.file_path = file_path
         self.tag = tag
+        self.last_log_time = time.time()
+        self.start_time = datetime.datetime.now()
+
         pass
 
     def __str__(self):
@@ -132,23 +131,6 @@ class MediaDownloadTask(object):
         offset = temp_path.stat().st_size if temp_path.exists() else 0
 
         logger.info(f"Start downloading {file_path} from offset {offset}")
-        start_time = datetime.datetime.now()
-
-        last_progress_time = asyncio.get_event_loop().time()
-
-        def callback(current, total):
-            nonlocal last_progress_time
-            last_progress_time = asyncio.get_event_loop().time()
-            logger.debug(f"{file_path} : {current} / {total}")
-            if self.on_downloader_net_callback is not None:
-                self.on_downloader_net_callback(
-                    self.task_id,
-                    f"{self.chat_name} - {file_path.name} - {self.tag}",
-                    start_time,
-                    datetime.datetime.now(),
-                    current,
-                    total
-                )
 
         # 获取文件大小（如果能获取）
         file_size = None
@@ -167,11 +149,20 @@ class MediaDownloadTask(object):
 
             try:
                 while True:
+                    start_time = time.time()
                     # 给每个 chunk 的接收设置超时，比如 100 秒
                     chunk = await asyncio.wait_for(stream.__anext__(), timeout=100)
                     f.write(chunk)
                     offset += len(chunk)
-                    callback(offset, file_size or 0)
+                    time_use = time.time() - start_time
+                    self.on_task_net_stat_event(
+                        file_path,
+                        offset,
+                        file_size,
+                        time_use,
+                        len(chunk)
+                    )
+
             except StopAsyncIteration:
                 # 下载完成
                 pass
@@ -186,31 +177,32 @@ class MediaDownloadTask(object):
         temp_path.rename(file_path)
         logger.info(f"Download finished: {file_path}")
 
-    async def download_direct(self, client, chat_name):
+    def on_task_net_stat_event(self, file_path,
+                               recv_bytes: int,
+                               total_bytes: int,
+                               time_use:float,
+                               time_recv:int):
+        try:
+            if time.time() - self.last_log_time > 10:
+                self.last_log_time = time.time()
+                status_show = {
+                    "task_tag": f"{self.chat_name} - {file_path.name} - {self.tag}",
+                    "time_use": str(datetime.datetime.now() - self.start_time),
+                    "last_recv_time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    "progress": f'{recv_bytes / total_bytes:.2%} ({recv_bytes / 1024 / 1024:.2f} MB / {total_bytes / 1024 / 1024:.2f} MB)',
+                    "speed": f'{(time_recv / 1024 / time_use):.5f} KB/s',
+                }
+                logger.info(f"\n{self.chat_name} stat:\n {pformat(status_show)}")
+        except Exception as e:
+            logger.error(f"on downloader net callback error: {e}")
+
+    async def download_direct(self, client):
         """
         直接下载, 不经过下载管理器
         :return:
         """
-        last_log_time = time.time()
-
-        def on_task_net_stat_event(task_id: int, task_tag: str, start_time: datetime.datetime,
-                                   last_recv_time: datetime.datetime, recv_bytes: int,
-                                   total_bytes: int):
-            # 每 5s 输出下信息
-            nonlocal last_log_time
-            if time.time() - last_log_time > 10:
-                last_log_time = time.time()
-                status_show = {
-                    "task_tag": task_tag,
-                    "time_use": str(datetime.datetime.now() - start_time),
-                    "last_recv_time": last_recv_time.strftime("%Y-%m-%d %H:%M:%S"),
-                    "progress": f'{recv_bytes / total_bytes:.2%} ({recv_bytes / 1024 / 1024:.2f} MB / {total_bytes / 1024 / 1024:.2f} MB)',
-                }
-                logger.info(f"\n{chat_name} stat:\n {pformat(status_show)}")
-
         await self.on_task_create()
         try:
-            self.on_downloader_net_callback = on_task_net_stat_event
             await self.download(client)
             await self.on_task_finished()
         except Exception as e:
@@ -307,10 +299,11 @@ class ChatMediaDownloader:
                 return True
             logger.warning(
                 f"target file exists but size not match, try continue {target_save_path.stat().st_size}/{media_size}: {target_save_path}")
+            target_save_path.unlink(missing_ok=True)
 
         task = MediaDownloadTask(msg_id, 3, 180, self.chat_id, self.chat_name, media_name, message, target_save_path,
                                  tag)
-        await task.download_direct(client=self.client, chat_name=self.chat_name)
+        await task.download_direct(client=self.client)
         return False
 
     async def download_all_media(self):
@@ -336,7 +329,7 @@ class ChatMediaDownloader:
         tmp_ids.append(s.max_finished_id)
         tmp_ids.extend(s.down_fail_ids)
         min_id = min(tmp_ids)
-        min_id = max(0, min_id - 5)  # 防止漏
+        min_id = max(0, min_id - 2)  # 防止漏
         logger.info(f"{self.chat_name}:  min_id: {min_id}")
         s.max_finished_id = min_id
         s.downloading_ids.clear()
@@ -349,7 +342,7 @@ class ChatMediaDownloader:
             try:
                 already_finished = await self.download_msg(message, f"{count}/{total_messages}")
                 if already_finished:
-                    DownloadingInfo.on_task_finish_impl(message.id, get_id_cache_path(self.chat_id),True)
+                    DownloadingInfo.on_task_finish_impl(message.id, get_id_cache_path(self.chat_id), True)
             except Exception as e:
                 logger.error(f"download fail {e}")
 
